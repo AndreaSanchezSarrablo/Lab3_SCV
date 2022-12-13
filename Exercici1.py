c0797c3e309059c8d19c0639e07d2bbc85605bd3
Andrea Sánchez <72571689+AndreaSanchezSarrablo@users.noreply.github.com>
2022-12-13 14:02:18 +0100

    Add files via upload

---
+++ Exercici1.py
@@ -0,0 +1,36 @@
+#EXERCICI 1
+import os
+
+#Creating a HLS transport stream container with a current video in the computer
+def ex1(input_video):
+	#Read input video and resized to multiple resolutions required (original video resolution, 1280x720 and 640x360
+	# Transcode the video to multiple bitrates for HLS packing
+	# Creating HLS Playlists (m3u8)
+	os.system('ffmpeg -i ' + str(input_video) + '.mp4 \
+	-filter_complex \
+	"[0:v]split=3[v1][v2][v3]; \
+	[v1]copy[v1out]; [v2]scale=w=1260:h=720[v2out]; [v3]scale=w=640:h=360[v3out]" \
+	-map [v1out] -c:v:0 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" ' \
+	  '-b:v:0 5M -maxrate:v:0 5M -minrate:v:0 5M -bufsize:v:0 10M -preset slow ' \
+	  '-g 48 -sc_threshold 0 -keyint_min 48 \
+	  -map [v2out] -c:v:1 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" ' \
+	  '-b:v:1 3M -maxrate:v:1 3M -minrate:v:1 3M -bufsize:v:1 3M -preset slow ' \
+	  '-g 48 -sc_threshold 0 -keyint_min 48 \
+	  -map [v3out] -c:v:2 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" ' \
+	  '-b:v:2 1M -maxrate:v:2 1M -minrate:v:2 1M -bufsize:v:2 1M -preset slow ' \
+	  '-g 48 -sc_threshold 0 -keyint_min 48 \
+	  -map a:0 -c:a:0 aac -b:a:0 96k -ac 2 \
+	  -map a:0 -c:a:1 aac -b:a:1 96k -ac 2 \
+	  -map a:0 -c:a:2 aac -b:a:2 48k -ac 2 \
+	  -f hls \
+	  -hls_time 2 \
+	  -hls_playlist_type vod \
+	  -hls_flags independent_segments \
+	  -hls_segment_type mpegts \
+	  -hls_segment_filename stream_%v/data%02d.ts \
+	  -master_pl_name master.m3u8 \
+	  -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" stream_%v.m3u8""')
+
+
+#Finally we call the function with the input video
+ex1("1min_cuttedVideo")
